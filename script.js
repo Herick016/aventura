45e130eb2383e4cc54a997681771b5bdcd9561d1
Herick016 <herick.cavalli@escola.pr.gov.br>
2025-09-04 09:05:11 -0300

    ok

---
+++ script.js
@@ -0,0 +1,19 @@
+const avanca =document.querySelectorA11 ('.btn-proximo');
+const reiniciarBtn = document.getElementById('.btn.reiniciar');
+
+avanca.forEach(button=> {
+button.addEventListener('click'function(){
+    const atual = document.querySelector('.ativo');
+    const proximoPsso='passo-'+this.getAttribute('data-proximo');
+    
+})
+
+
+
+
+}
+
+
+
+
+) 
